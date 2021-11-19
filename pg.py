c12e5ff9dda8794cec13eab93959d503aaa5dd76
Shamil Dashapylov <shom4ik@icloud.com>
2021-11-19 17:56:19 +0300

    Update pg.py
    Add input for users

--- pg.py
+++ pg.py
@@ -6,7 +6,7 @@ numbers = '0123456789'
 symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
 all = lower + upper + numbers + symbols
-length = 16
+length = int(input('Enter the length of the password: '))
 
 password = ''.join(random.sample(all, length))
 
