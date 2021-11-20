3982f0785474b86073d6dbb9aa01aa29a78815dc
Shamil Dashapylov <shom4ik@icloud.com>
2021-11-20 11:19:32 +0300

    update pg.py
    add check for int/string in message user

--- pg.py
+++ pg.py
@@ -8,28 +8,27 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привет!\nЯ PassBot!\nСоздан в ASPIRE.")
+    await message.reply("Привет!\nВведи длину символов пароля от 3 до 30")
 
-@dp.message_handler(commands=['help'])
-async def send_welcome(message: types.Message):
-    await message.reply("Используй команду\n /gen")
-
-@dp.message_handler(commands=['generate', 'gen'])
+@dp.message_handler()
 async def echo(message: types.Message):
+    if message.text.isdigit():
+        lower = 'abcdefghijklmnopqrstuvwxyz'
+        upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        numbers = '0123456789'
+        symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
 
-    lower = 'abcdefghijklmnopqrstuvwxyz'
-    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    numbers = '0123456789'
-    symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    
-    all = lower + upper + numbers + symbols
-    length = 16
-    
-    password = ''.join(random.sample(all, length))
 
-    await message.reply('Ваш новый пароль: ' + password)
+        all = lower + upper + numbers + symbols
+        length = int(message.text)
+        
+        password = ''.join(random.sample(all, length))
+
+        await message.answer('Ваш новый пароль: ' + password)
+    else:
+        await message.answer('Введите цифру обозначающую длину пароля от 1 до 30')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
