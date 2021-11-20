121c0076e42123722587c2bae0933d0b05a1d462
Shamil Dashapylov <shom4ik@icloud.com>
2021-11-20 10:43:01 +0300

    Update pg.py
    Add telegram bot functional on aiogram module

--- pg.py
+++ pg.py
@@ -1,13 +1,35 @@
 import random
+import logging
+from aiogram import Bot, Dispatcher, executor, types
 
-lower = 'abcdefghijklmnopqrstuvwxyz'
-upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-numbers = '0123456789'
-symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+API_TOKEN = '2114579841:AAFRsgLhy200qbHs_WqD10HLVmJRFHGbs5A'
+logging.basicConfig(level=logging.INFO)
 
-all = lower + upper + numbers + symbols
-length = int(input('Enter the length of the password: '))
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-password = ''.join(random.sample(all, length))
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\nЯ PassBot!\nСоздан в ASPIRE.")
 
-print(password)
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Используй команду\n /gen")
+
+@dp.message_handler(commands=['generate', 'gen'])
+async def echo(message: types.Message):
+
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numbers = '0123456789'
+    symbols = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    
+    all = lower + upper + numbers + symbols
+    length = 16
+    
+    password = ''.join(random.sample(all, length))
+
+    await message.reply('Ваш новый пароль: ' + password)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
